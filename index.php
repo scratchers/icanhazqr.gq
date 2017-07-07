eba571152417e4e31f35b63af77fb4e19f82be62
Jeff Puckett <jeff@jeffpuckett.com>
2017-07-07 02:51:55 +0000

    add function imagecopymerge_alpha
    https://stackoverflow.com/q/42731201/4233593

--- index.php
+++ index.php
@@ -18,3 +18,17 @@ imagecopymerge_alpha($main, $qr, 0, 0, 0, 0, 150, 150, 100);
 imagecopymerge_alpha($main, $im, 0, 150, 0, 0, 150, 30, 100);
 imagepng($main);
 imagedestroy($main);
+
+function imagecopymerge_alpha($dst_im, $src_im, $dst_x, $dst_y, $src_x, $src_y, $src_w, $src_h, $pct){
+    // creating a cut resource
+    $cut = imagecreatetruecolor($src_w, $src_h);
+
+    // copying relevant section from background to the cut resource
+    imagecopy($cut, $dst_im, 0, 0, $dst_x, $dst_y, $src_w, $src_h);
+
+    // copying relevant section from watermark to the cut resource
+    imagecopy($cut, $src_im, 0, 0, $src_x, $src_y, $src_w, $src_h);
+
+    // insert cut resource to destination image
+    imagecopymerge($dst_im, $cut, $dst_x, $dst_y, 0, 0, $src_w, $src_h, $pct);
+}
